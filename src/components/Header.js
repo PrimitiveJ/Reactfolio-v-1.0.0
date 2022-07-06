972ce5677efd33f0ac91ef8a2a8837bc93dbf386
Primitive J <hokecj@gmail.com>
2022-07-06 18:27:25 -0400

    used bootstrap for new completed navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,29 +4,30 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
-            <nav>
-                <div className="logo">
-                    <h1>Christopher J Hoke/James L Primitive</h1>
-                </div>
+           <nav class="navbar navbar-expand-sm bg-light">
 
-                <ul className="nav-items">
-                    <li>
-                       <a href="home">Home</a>
-                    </li>
-                    <li>
-                       <a href="portfolio">Portfolio</a>
-                    </li>
-                    <li>
-                       <a href="aboutme">About Me</a>
-                    </li>
-                    <li>
-                       <a href="contact">Contact</a>
-                    </li>
-                    <li>
-                       <a href="resume">Resume</a>
-                    </li>
-                </ul>
-            </nav>
+<div class="container-fluid">
+  Christopher James Hoke
+  <ul class="navbar-nav">
+  <li class="nav-item">
+      <a class="nav-link" href="home">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="aboutme">About Me</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="portfolio">Portfolio</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="contact">Contact</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="resume">Resume</a>
+    </li>
+  </ul>
+</div>
+
+</nav>
         </div>
     )
 }
